779bb026e1f024d313bb69fbb705b536ca28017c
denishamann@googlemail.com <denishamann@googlemail.com@54a2982c-f802-5f40-2f93-e7e75747859a>
2011-06-01 14:59:57 +0000

    *FIX: -some bugs

--- tire.js
+++ tire.js
@@ -19,7 +19,7 @@
   sPuffer="";  
   }else{
 
-	var strReg = /(P|LT|T)?(\d{2}x)?([1-9][1234567890\.,]{1,4}\s?)[\/|\s]?([1234567890\.,]{2,5})?\s*([BDFHRVZ-]{1,2}(?=[1-9]))\s*([1-9][1234567890\.,]{1,4})\s*(LT|CP|C?)\s*(\d{1,2}\s?PR)?\s*(M\/C)?\s*(XL|EL|Reinf|rf\.|RF)?\s*((\()?(\d{2,3})?\/?(\d{2,3})\s*(A[0-8]?|[BCDEFGJKLMNPQRSTUHVWY])(\))?)?/gi;
+	var strReg = /(P|LT|T)?(\d{2}x)?([1-9][1234567890\.,]{1,4}\s?)[\/|\s]?([1234567890\.,]{2,5})?\s*([BDFHRSVZ-]{1,2}(?=[1-9]))\s*([1-9][1234567890\.,]{1,4})\s*(LT|CP|C?)\s*(\d{1,2}\s?PR)?\s*(M\/C)?\s*(XL|EL|Reinf|rf\.|RF)?\s*((\()?(\d{2,3})?\/?(\d{2,3})\s*(A[0-8]?|[BCDEFGJKLMNPQRSTUHVWY])(\))?)?/gi;
 
 	var patt1=new RegExp(strReg);
 	sPuffer=sPuffer.match(patt1);
@@ -39,7 +39,7 @@
   sPuffer="";  
   }else{
 
-	var strReg = /(P|LT|T)?(\d{2}x)?([1-9][1234567890\.,]{1,4}\s?)[\/|\s]?([1234567890\.,]{2,5})?\s*([BDFHRVZ-]{1,2}(?=[1-9]))\s*([1-9][1234567890\.,]{1,4})\s*(LT|CP|C?)\s*(\d{1,2}\s?PR)?\s*(M\/C)?\s*(XL|EL|Reinf|rf\.|RF)?\s*((\()?(\d{2,3})?\/?(\d{2,3})\s*(A[0-8]?|[BCDEFGJKLMNPQRSTUHVWY])(\))?)?/gi;
+	var strReg = /(P|LT|T)?(\d{2}x)?([1-9][1234567890\.,]{1,4}\s?)[\/|\s]?([1234567890\.,]{2,5})?\s*([BDFHRSVZ-]{1,2}(?=[1-9]))\s*([1-9][1234567890\.,]{1,4})\s*(LT|CP|C?)\s*(\d{1,2}\s?PR)?\s*(M\/C)?\s*(XL|EL|Reinf|rf\.|RF)?\s*((\()?(\d{2,3})?\/?(\d{2,3})\s*(A[0-8]?|[BCDEFGJKLMNPQRSTUHVWY])(\))?)?/gi;
 
 	var patt1=new RegExp(strReg);
 	
@@ -70,7 +70,7 @@
   sPuffer="";  
   }else{
 
-	var strReg = /(P|LT|T)?(\d{2}x)?([1-9][1234567890\.,]{1,4}\s?)[\/|\s]?([1234567890\.,]{2,5})?\s*([BDFHRVZ-]{1,2}(?=[1-9]))\s*([1-9][1234567890\.,]{1,4})\s*(LT|CP|C?)\s*(\d{1,2}\s?PR)?\s*(M\/C)?\s*(XL|EL|Reinf|rf\.|RF)?\s*((\()?(\d{2,3})?\/?(\d{2,3})\s*(A[0-8]?|[BCDEFGJKLMNPQRSTUHVWY])(\))?)?/gi;
+	var strReg = /(P|LT|T)?(\d{2}x)?([1-9][1234567890\.,]{1,4}\s?)[\/|\s]?([1234567890\.,]{2,5})?\s*([BDFHRSVZ-]{1,2}(?=[1-9]))\s*([1-9][1234567890\.,]{1,4})\s*(LT|CP|C?)\s*(\d{1,2}\s?PR)?\s*(M\/C)?\s*(XL|EL|Reinf|rf\.|RF)?\s*((\()?(\d{2,3})?\/?(\d{2,3})\s*(A[0-8]?|[BCDEFGJKLMNPQRSTUHVWY])(\))?)?/gi;
 
 	var patt1=new RegExp(strReg);
 	
@@ -78,7 +78,14 @@
 	
 	for(var i=0;i<sPuffer.match(patt1).length;i++)
 	{
-	//alert(i);
+	//alert("test");
+	
+	// alert(patt1.exec(sPuffer.match(patt1)[i])[0]);
+	// alert(patt1.exec(sPuffer.match(patt1)[i])[1]);
+	// alert(patt1.exec(sPuffer.match(patt1)[i])[2]);
+	// alert(patt1.exec(sPuffer.match(patt1)[i])[3]);
+	// alert(patt1.exec(sPuffer.match(patt1)[i])[4]);
+	
 	// <form method='POST' action='SAPEVENT:TYRE_SIZE_" & getTyre(TMPRA1) & "_'><input type='submit' value='" & getTyreDesc(TMPRA1) & "'></form>
 	srPuffer=srPuffer.replace(new RegExp(sPuffer.match(patt1)[i],"gi"),"<a href='SAPEVENT:TYRE_SIZE_"+sPuffer.match(patt1)[i]+"'>"+sPuffer.match(patt1)[i]+"</a>");
 	}
